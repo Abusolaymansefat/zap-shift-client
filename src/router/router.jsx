dd45b6075efb8e500d26ec004af6f3d11687e405
Abusolay <sefat01625@gmail.com>
2025-06-26 01:46:54 +0600

    map use

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,35 +1,39 @@
-import {
-  createBrowserRouter,
-} from "react-router";
+import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../Pages/Home/Home/Home";
 import AuthLayout from "../layouts/AuthLayout";
 import Login from "../Pages/Authentications/Login/Login";
 import Register from "../Pages/Authentications/Register/Register";
+import Coverage from "../Pages/Coverage/Coverage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
-    children:[
-        {
-            index: true,
-            Component: Home
-        }
-    ]
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "coverage",
+        Component: Coverage,
+        loader:() => fetch('./warehouses.json')
+      },
+    ],
   },
   {
-    path: '/',
+    path: "/",
     Component: AuthLayout,
     children: [
-        {
-            path: 'login',
-            Component: Login,
-        },
-        {
-            path: 'register',
-            Component: Register
-        }
-    ]
-  }
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "register",
+        Component: Register,
+      },
+    ],
+  },
 ]);
